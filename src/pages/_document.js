79fc463db90c6786009323ea54fed82ad85d933e
Emperor D. Pengin <emperord.pengin@gmail.com>
2024-05-13 23:55:52 -0400

    second

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// src/pages/index.js
+import Head from 'next/head';
+import Link from 'next/link';
+import '../styles/globals.scss';
 
-export default function Document() {
+export default function Home() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    <div className="container">
+      <Head>
+        <title>Fantasy Notebook</title>
+      </Head>
+      <h1>INDEX</h1>
+      <input type="text" className="search-box" placeholder="Search pages..." />
+      <ul>
+        <li><Link href="/notes/lesson1"><a>Lesson 1: Introduction to Magic</a></Link></li>
+        <li><Link href="/notes/lesson2"><a>Lesson 2: Advanced Potions</a></Link></li>
+        <li><Link href="/notes/lesson3"><a>Mystical Creatures</a></Link></li>
+      </ul>
+    </div>
   );
 }
